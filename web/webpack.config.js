d19e3aa3dd71e5574696c4eac55fe51703bec788
Afshin Mehrabani <afshin.meh@gmail.com>
2017-09-30 17:47:58 +0100

    fix: adding web font, adding package lock, fixing webpack

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     'webpack-hot-middleware/client',
     './src/index'
   ],
+  externals:[{
+    xmlhttprequest: '{XMLHttpRequest:XMLHttpRequest}'
+  }],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
